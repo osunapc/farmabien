d7f2201ae67bb784e87bb7d5f401df9bea904bf0
osunapc <osunapc@gmail.com>
2024-11-07 12:03:42 -0400

    reparacion evn

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,3 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-	process.env.PUBLIC_SUPABASE_URL,
-	process.env.PUBLIC_SUPABASE_ANON_KE
-);
+export const supabase = createClient(import.meta.env.PUBLIC_SUPABASE_URL, import.meta.env.PUBLIC_SUPABASE_ANON_KEY);
