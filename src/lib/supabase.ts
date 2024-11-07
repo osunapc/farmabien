609db4c72c6c91bbbbd893f6b3f69267e8c78b1b
osunapc <osunapc@gmail.com>
2024-11-07 11:55:06 -0400

    corrigiendo env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-	"https://pauoqbnvqbjrygrsbonc.supabase.co",
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBhdW9xYm52cWJqcnlncnNib25jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA1ODcwNzcsImV4cCI6MjA0NjE2MzA3N30.4IsFtyIfYsH_llYts__C5gyzod6Hy_KIAGDcrwue4dY"
+	process.env.PUBLIC_SUPABASE_URL,
+	process.env.PUBLIC_SUPABASE_ANON_KE
 );
